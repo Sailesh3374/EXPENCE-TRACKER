1407ff1008ebf9af40ae0c7e9aa90c212c6ece02
Sailesh Dhakne <159609192+Sailesh3374@users.noreply.github.com>
2024-12-19 00:30:14 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,48 @@
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.3.15")
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.expensestracker"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.expensestracker"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.7.2" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.google.firebase:firebase-auth:22.1.1")
+
+    implementation ("com.google.firebase:firebase-core:21.1.1")
+    implementation ("com.firebase:firebase-client-android:2.5.2")
+    implementation ("com.firebaseui:firebase-ui-database:8.0.2")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.google.firebase:firebase-database:20.2.2")
+
 }
-
-
